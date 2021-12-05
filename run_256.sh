9d3e943ce66bf84db221d59fb7f03e0ead62b459
RuchithaManne <68728304+RuchithaManne@users.noreply.github.com>
2021-12-06 00:22:48 +0530

    colab_

--- run_256.sh
+++ run_256.sh
@@ -6,7 +6,7 @@
 #$ -S /bin/sh
 #
 
-CUDA_VISIBLE_DEVICES=0 python /content/IntroVAE/main.py  --hdim=512 --output_height=256 --channels='32, 64, 128, 256, 512, 512' --m_plus=120 --weight_rec=0.05 --weight_kl=1.0  --weight_neg=0.5 --num_vae=0  --dataroot='/home/huaibo.huang/data/celeba-hq/celeba-hq-images-256' --trainsize=29000 --test_iter=1000 --save_iter=1 --start_epoch=0  --batchSize=16 --nrow=8 --lr_e=0.0002 --lr_g=0.0002   --cuda  --nEpochs=500   2>&1   | tee train0.log 
+CUDA_VISIBLE_DEVICES=0 python /content/IntroVAE/main.py  --hdim=512 --output_height=256 --channels='32, 64, 128, 256, 512, 512' --m_plus=120 --weight_rec=0.05 --weight_kl=1.0  --weight_neg=0.5 --num_vae=0  --dataroot='/content/drive/MyDrive/CelebA_dataset/img_align_celeba/img_align_celeba' --trainsize=29000 --test_iter=1000 --save_iter=1 --start_epoch=0  --batchSize=16 --nrow=8 --lr_e=0.0002 --lr_g=0.0002   --cuda  --nEpochs=500   2>&1   | tee train0.log 
 
 #Another appropriate setting defined as below:
 
